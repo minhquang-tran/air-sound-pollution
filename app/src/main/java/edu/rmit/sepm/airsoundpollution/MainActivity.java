a727c38b00749a1ddf3dc1f8256864c11ad4ab9e
quang_tran_minh_ <s3410417@gmail.com>
2016-07-20 14:47:12 +0700

    Initial layout & method placeholders

--- app/src/main/java/edu/rmit/sepm/airsoundpollution/MainActivity.java
+++ app/src/main/java/edu/rmit/sepm/airsoundpollution/MainActivity.java
@@ -2,6 +2,8 @@ package edu.rmit.sepm.airsoundpollution;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void connect_bluetooth(View view) {
+        //bluetooth connection handling here
+
+        //example on changing textview
+        TextView status = (TextView) findViewById(R.id.status_text);
+        status.setText(getString(R.string.status_pressed));
+
+        //Alternate short version
+        //((TextView) findViewById(R.id.status_text)).setText(getString(R.string.status_pressed));
+
+
+    }
+
+    public void receive_data(View view) {
+        //change name to temp.* so device can create a new file to write
+
+        //read data from file
+
+        //update local storage
+
+    }
+
+    public void view_data(View view) {
+    }
+
+    public void upload_data(View view) {
     }
 }
